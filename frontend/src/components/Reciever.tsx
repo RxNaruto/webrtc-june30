5e24ec44b9c40b4a4aec1f6df326229f7493486d
Rithk <rithkchaudhary86@gmail.com>
2025-07-01 15:45:31 +0530

    turn-server

--- frontend/src/components/Reciever.tsx
+++ frontend/src/components/Reciever.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const Receiver = () => {
     const [socket, setSocket] = useState<null | WebSocket>(null);
-
+    const [peerConfig, setPeerConfig] = useState<any>(null);
 
     useEffect(() => {
         const socket = new WebSocket('wss://webrtc2way.rithkchaudharytechnologies.xyz/ws/');
@@ -10,21 +10,18 @@ export const Receiver = () => {
             socket.send(JSON.stringify({ type: 'receiver' }));
         };
         setSocket(socket);
+
+        (async () => {
+            const response = await fetch("https://mycapstoneturnserver.metered.live/api/v1/turn/credentials?apiKey=49c30365ef3c75870c2e02da41af28ba0a40");
+            const iceServers = await response.json();
+            setPeerConfig({ iceServers });
+        })();
     }, []);
 
     async function startSendingVideo() {
-        if (!socket) return;
+        if (!socket || !peerConfig) return;
 
-        const peerConnection = new RTCPeerConnection({
-            iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    {
-        urls: 'turn:relay1.expressturn.com:3478',
-        username: 'efgh',
-        credential: 'efgh'
-    }
-]
-        });
+        const peerConnection = new RTCPeerConnection(peerConfig);
 
         peerConnection.onicecandidate = (event) => {
             if (event.candidate) {
